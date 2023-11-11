5ba72df0ae0aedf3d3f84f0e044c43b22886cdd8
Neo-Diamons <romain.hure@epitech.eu>
2023-11-11 14:42:18 +0100

    fix(main): improve style of the floating coords.

--- src/pages/Background.tsx
+++ src/pages/Background.tsx
@@ -5,9 +5,12 @@ function Background() {
     const coords = getMousePosition();
 
     return (
-        <div className="block bg-gray-950 min-w-screen min-h-screen">
+        <div className="group block bg-gray-950 min-w-screen min-h-screen hover:cursor-none">
             <p
-                className="absolute text-white transform -translate-x-1/2 -translate-y-1/2 hover:cursor-none"
+                className="
+                    absolute text-white transform -translate-x-1/2 -translate-y-1/2
+                    group-hover:block hidden whitespace-nowrap select-none
+                "
                 style={{
                     left: coords.x,
                     top: coords.y,
