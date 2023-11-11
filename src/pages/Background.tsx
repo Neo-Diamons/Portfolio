cf614f2fad26a074c765e18087de605c6f4a28ef
Neo-Diamons <romain.hure@epitech.eu>
2023-11-11 19:33:42 +0100

    feat(main): add cursor.

--- src/pages/Background.tsx
+++ src/pages/Background.tsx
@@ -21,18 +21,21 @@ function Background() {
         ">
             <div className="hidden group-hover:block min-w-screen min-h-screen">
                 <div
+                    className="absolute w-4 h-4 text-center bg-white rounded-full blur-sm"
+                    style={{ left: coords.x - 8, top: coords.y - 8 }}
+                />
+
+                <div
                     className="
                         absolute text-white transform -translate-x-1/2 -translate-y-1/2
                         bg-gray-900 bg-opacity-75 rounded-md px-2 py-1
                         whitespace-nowrap select-none
                     "
-                    style={{
-                        left: window.innerWidth / 2,
-                        top: 25,
-                    }}
+                    style={{ left: window.innerWidth / 2, top: 25, }}
                 >
                     <h1>({coords.x}, {coords.y})</h1>
                 </div>
+
                 <svg viewBox={"0 0 " + window.innerWidth + " " + window.innerHeight} xmlns="http://www.w3.org/2000/svg">
                     {quadTree.map((quad, index) => (
                         <rect
